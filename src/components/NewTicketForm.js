0ae9c02ad06d1b28824593f64f7c5eec70d5012f
Jesse White <jesse.white6@gmail.com>
2021-06-28 16:10:43 -0700

    :beers: add show and hide elements to the page.

---
+++ src/components/NewTicketForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewTicketForm(prop) {
+  return (
+    <React.Fragment>
+      <h3>This is a form boi.</h3>
+    </React.Fragment>
+  )
+}
+
+export default NewTicketForm;
