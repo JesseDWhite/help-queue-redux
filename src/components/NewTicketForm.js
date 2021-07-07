0ed6171838761cb3c3224f66352a6aae7a8f8881
Jesse White <jesse.white6@gmail.com>
2021-07-07 08:06:13 -0700

    :busts_in_silhouette: refactor NewTicketForm and add contributor.
    Co-authored-by: Jeremy Banka <hello@jeremybanka.com>

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -4,12 +4,13 @@ import PropTypes from "prop-types";
 import ReusableForm from "./ReusableForm";
 
 function NewTicketForm({ onNewTicketCreation }) {
-  function handleNewTicketFormSubmission(event) {
-    event.preventDefault();
+  function handleNewTicketFormSubmission(e) {
+    const { names, location, issue } = e.target;
+    e.preventDefault();
     onNewTicketCreation({
-      names: event.target.names.value,
-      location: event.target.location.value,
-      issue: event.target.issue.value,
+      names: names.value,
+      location: location.value,
+      issue: issue.value,
       id: v4(),
     });
   }
